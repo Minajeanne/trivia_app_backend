17f32362cc8209d67933a9795ac11e8d37edae96
Minajeanne <albrown07@mac.com>
2019-09-23 14:39:24 -0400

    create pg db & migrate

--- db/migrate/20190921180020_create_questions.rb
+++ db/migrate/20190921180020_create_questions.rb
@@ -1,7 +1,14 @@
 class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
-    
+      t.integer "game_id"
+      t.string "question_text"
+      t.string "answer_one"
+      t.string "answer_two"
+      t.string "answer_three"
+      t.string "answer_four"
+      t.integer "correct_answer"
+
       t.timestamps
     end
   end
