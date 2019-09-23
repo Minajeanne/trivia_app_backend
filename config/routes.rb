ad9f5c60d37c1d257243c720affbf50071b070be
Minajeanne <albrown07@mac.com>
2019-09-23 14:01:40 -0400

    namespace routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :questions
-  resources :games
+  # post "/api/v1/login", to: "api/v1/sessions#create"
+  #  post "/api/v1/signup", to: "api/v1/users#create"
+  #  delete "/api/v1/logout", to: "api/v1/sessions#destroy"
+  #  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+  namespace :api do
+    namespace :v1 do
+      resources :questions
+      resources :games
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
