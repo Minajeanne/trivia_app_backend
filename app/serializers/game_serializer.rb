25b193b1d15e3510031d2837701bc97173b650ee
Minajeanne <albrown07@mac.com>
2019-09-24 16:25:53 -0400

    add attributes

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,4 +1,17 @@
 class GameSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id
+
+  attribute :questions do |game|
+    game.questions.map do |question|
+      {
+        question_text: question.question_text,
+        answer_one: question.answer_one,
+        answer_two: question.answer_two,
+        answer_three: question.answer_three,
+        answer_four: question.answer_four,
+        correct_answer: question.correct_answer
+      }
+    end
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,7 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :username
+  has_many :games, serializer: GameSerializer
+  has_many :stats, serializer: StatSerializer
+
 end
