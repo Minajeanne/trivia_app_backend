2bf41d3cef1fb7b06fe1eefd5ba6d8f01b5821fe
Minajeanne <albrown07@mac.com>
2019-09-26 16:08:11 -0400

    stub out controller methods

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,3 +1,71 @@
 class Api::V1::GamesController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_game, only: [:show, :update, :destroy]
+
+# GET /trips
+def index
+  if logged_in?
+    @games = current_user.games
+
+    render json: GameSerializer.new(@games)
+  else
+    render json: {
+      error: "You must be logged in to see your games"
+    }
+  end
+end
+
+# GET /trips/1
+def show
+  render json: @game
+end
+
+# POST /trips
+def create
+  # byebug
+  @game = current_user.games.build(game_params)
+
+  if @game.save
+    render json:  GameSerializer.new(@game), status: :created
+  else
+    error_resp = {
+      error: @game.errors.full_messages.to_sentence
+    }
+    render json: error_resp, status: :unprocessable_entity
+  end
+end
+
+# PATCH/PUT /trips/1
+def update
+  if @game.update(game_params)
+    render json:  GameSerializer.new(@game), status: :ok
+  else
+    error_resp = {
+      error: @game.errors.full_messages.to_sentence
+    }
+    render json: error_resp, status: :unprocessable_entity
+  end
+end
+
+# DELETE /trips/1
+def destroy
+  if @game.destroy
+    render json:  { data: "Game successfully deleted" }, status: :ok
+  else
+    error_resp = {
+      error: "Game not found and not deleted"
+    }
+    render json: error_resp, status: :unprocessable_entity
+  end
+end
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def game_params
+    params.require(:game).permit(:user_id)
+  end
 end
--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,3 +1,51 @@
 class Api::V1::QuestionsController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
+
+
+ def index
+   @questions = Question.all
+   # @questions = Question.all.select{ |q| q.author_id == @user.id}
+    # if @questions
+    #   render json: @questions
+   render json: @questions
+  else
+    render json: @question.errors, status: :unprocessable_entity
+ end
+
+ def show
+   render json: @guestion
+ end
+
+ def create
+   @question = Question.new(question_params)
+
+   if @question.save
+     render json: @question, status: :created
+   else
+     render json: @question.errors, status: :unprocessable_entity
+   end
+ end
+
+ def update
+   if @question.update(question_params)
+     render json: @question
+   else
+     render json: @question.errors, status: :unprocessable_entity
+   end
+ end
+
+ def destroy
+   @question.destroy
+ end
+
+ private
+   # Use callbacks to share common setup or constraints between actions.
+   def set_question
+     @question = Question.find(params[:id])
+   end
+
+   # Only allow a trusted parameter "white list" through.
+   def question_params
+     params.require(:question).permit(:game_id, :question_text, :answer_one, :answer_two, :answer_three, :answer_four, :correct_answer)
+   end
 end
