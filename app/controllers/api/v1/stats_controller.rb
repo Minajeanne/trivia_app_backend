209ecca231564c50990ccbcfc036075d610037d4
Minajeanne <albrown07@mac.com>
2019-09-26 16:12:40 -0400

    stub out stats controller methods

--- app/controllers/api/v1/stats_controller.rb
+++ app/controllers/api/v1/stats_controller.rb
@@ -1,3 +1,26 @@
 class Api::V1::StatsController < ApplicationController
-  # before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:index, :show, :destroy]
+
+  def index
+    User.all.each do |user|
+      Stat.create_or_update_user_stats(user)
+    end
+   @stats = Stat.all.sort_by{ |s| s.ranking }
+   
+   render json: @stats
+ end
+
+  def show
+    render json: @user.stat
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(id: params[:user_id])
+  end
+
+  def stat_params
+    params.require(:stat).permit(:user_id, :longest_streak, :correct_overall, :incorrect_overall, :ranking)
+  end
 end
