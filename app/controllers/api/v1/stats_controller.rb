c18262fa3cc559a48fd3beac4da831d67c43e0f8
Minajeanne <albrown07@mac.com>
2020-03-26 13:37:46 -0400

    added post route to stats#index

--- app/controllers/api/v1/stats_controller.rb
+++ app/controllers/api/v1/stats_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::StatsController < ApplicationController
   def index
     @stats = Stat.all
     @users_rankings = Stat.create_or_update_user_rank
-# binding.pry
+
     render json: @users_rankings.as_json(
       only: [:user_id, :total_correct, :user_pr, :user_rank],
       include: { user: { only: [:username] } }
@@ -20,12 +20,12 @@ class Api::V1::StatsController < ApplicationController
   def update
     score = params[:score]
     high_score = @user.stat.total_correct
-    ranking = params[:user_rank]
+
     @user = User.find_by(id: params[:id])
 
-    if @user.stat.user_rank.nil? || ranking > @user.stat.user_rank
-      Stat.create_or_update_user_rank
-    end
+    # if @user.stat.user_rank.nil? || ranking > @user.stat.user_rank
+    #   Stat.create_or_update_user_rank
+    # end
 
     if @user.stat.total_correct.nil? || score > @user.stat.total_correct
       @user.stat.update(total_correct: score, user_pr: high_score)
